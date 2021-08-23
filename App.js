e9a4b2b0c8cf7ac2fb72299f11d6261ece1167b7
Brandon Aase <baase@catalyte.io>
2021-08-23 16:01:31 -0700

    Added first shaped candies.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Svg, { Circle, Polygon } from 'react-native-svg';
+import Svg, { Circle, Polygon, Rect } from 'react-native-svg';
 
 export default function App() {
   return (
@@ -31,25 +31,117 @@ export default function App() {
               <Svg height="100" width="100">
                 <Polygon
                   points="50,5 95,50 50,95 5,50"
-                  fill="#2b1d0e"
+                  fill="#3e2a14"
                   stroke="#f5f5dc"
                   strokeWidth="3"
                 />
                 <Polygon
                   points="50,5 95,50 50,95 5,50"
-                  fill="#2b1d0e"
+                  fill="#3e2a14"
                   stroke="#f5f5dc"
                   strokeWidth="3"
                   transform="rotate(30, 50, 50)"
                 />
                 <Polygon
                   points="50,5 95,50 50,95 5,50"
-                  fill="#2b1d0e"
+                  fill="#3e2a14"
                   stroke="#f5f5dc"
                   strokeWidth="3"
                   transform="rotate(60, 50, 50)"
                 />
-                <Circle cx="50" cy="50" r="37" fill="#2b1d0e" />
+                <Circle cx="50" cy="50" r="37" fill="#3e2a14" />
+                <Polygon
+                  points="50,15 80,70 20,70"
+                  stroke="#644420"
+                  strokeWidth="2"
+                />
+                <Polygon
+                  points="50,25 70,65 30,65"
+                  stroke="#644420"
+                  strokeWidth="2"
+                />
+                <Polygon
+                  points="50,37 60,60 40,60"
+                  stroke="#644420"
+                  strokeWidth="2"
+                />
+              </Svg>
+            </View>
+            <View>
+              <Svg height="100" width="100">
+                <Polygon
+                  points="50,5 95,50 50,95 5,50"
+                  fill="#ff8da1"
+                  stroke="#f5f5dc"
+                  strokeWidth="3"
+                />
+                <Polygon
+                  points="50,5 95,50 50,95 5,50"
+                  fill="#ff8da1"
+                  stroke="#f5f5dc"
+                  strokeWidth="3"
+                  transform="rotate(30, 50, 50)"
+                />
+                <Polygon
+                  points="50,5 95,50 50,95 5,50"
+                  fill="#ff8da1"
+                  stroke="#f5f5dc"
+                  strokeWidth="3"
+                  transform="rotate(60, 50, 50)"
+                />
+                <Circle cx="50" cy="50" r="37" fill="#ff8da1" />
+                <Circle cx="50" cy="50" r="30" stroke="#ff748c" strokeWidth="2" />
+                <Circle cx="50" cy="50" r="25" stroke="#ff748c" strokeWidth="2" />
+                <Circle cx="50" cy="50" r="20" stroke="#ff748c" strokeWidth="2" />
+              </Svg>
+            </View>
+            <View>
+              <Svg height="100" width="100">
+                <Polygon
+                  points="50,5 95,50 50,95 5,50"
+                  fill="#b67233"
+                  stroke="#f5f5dc"
+                  strokeWidth="3"
+                />
+                <Polygon
+                  points="50,5 95,50 50,95 5,50"
+                  fill="#b67233"
+                  stroke="#f5f5dc"
+                  strokeWidth="3"
+                  transform="rotate(30, 50, 50)"
+                />
+                <Polygon
+                  points="50,5 95,50 50,95 5,50"
+                  fill="#b67233"
+                  stroke="#f5f5dc"
+                  strokeWidth="3"
+                  transform="rotate(60, 50, 50)"
+                />
+                <Circle cx="50" cy="50" r="37" fill="#b67233" />
+                <Rect
+                  x="28"
+                  y="27"
+                  width="45"
+                  height="45"
+                  strokeWidth="2"
+                  stroke="#a3662d"
+                />
+                <Rect
+                  x="33"
+                  y="32"
+                  width="35"
+                  height="35"
+                  strokeWidth="2"
+                  stroke="#a3662d"
+                />
+                <Rect
+                  x="38"
+                  y="37"
+                  width="25"
+                  height="25"
+                  strokeWidth="2"
+                  stroke="#a3662d"
+                />
               </Svg>
             </View>
           </View>
