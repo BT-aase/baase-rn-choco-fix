a0d1ceecf7330ba72e774e08a8b003e48873a2a4
Brandon Aase <baase@catalyte.io>
2021-08-30 16:01:16 -0700

    Added visual part of Buttons.

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Buttons = (props) => {
+    console.log(props)
     return (
         <View style={styles.buttons}>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={props.openMenu}>
+                    <View style={styles.menu}>
+                        <Text style={styles.buttonText}>{props.menu.toString()}</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.order}>
+                        <Text style={styles.buttonText}>- Notes -</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.check}>
+                        <Icon name="checkmark-circle-outline" size={28} color='white'/>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 };
@@ -12,8 +31,51 @@ const styles = StyleSheet.create({
     buttons: {
         height: '8%',
         width: '100%',
-        backgroundColor: '#4a4a4a'
+        backgroundColor: '#4a4a4a',
     },
+    buttonContainer: {
+        height: '8%',
+        width: '100%',
+        marginTop: 5,
+        marginLeft: 15,
+        flexDirection: 'row'
+    },
+    menu: {
+        height: 50,
+        width: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#772424',
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: 'white',
+        marginHorizontal: 10
+    },
+    order: {
+        height: 50,
+        width: 110,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#772424',
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: 'white',
+        marginHorizontal: 10
+    },
+    check: {
+        height: 50,
+        width: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#007f00',
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: 'white',
+        marginHorizontal: 10
+    },
+    buttonText: {
+        color: 'white'
+    }
 });
 
 export default Buttons;
--- screens/Game.js
+++ screens/Game.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Modal, StyleSheet, View } from 'react-native';
 
 import Tray from '../components/Tray';
 import Counter from '../components/Counter';
 import Buttons from '../components/Buttons';
 
 const Game = (props) => {
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const openMenu = () => {
+    console.log('text');
+  }
+
   return (
     <View style={styles.container}>
-        <Tray />
-        <Counter />
-        <Buttons />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={menuVisible}
+      >
+        <Text>Menu</Text>
+      </Modal>
+      <Tray />
+      <Counter />
+      <Buttons openMenu={() => openMenu()} menu={menuVisible}/>
     </View>
   );
 }
