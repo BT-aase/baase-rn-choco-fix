30474348e5b51586d8b2119f7e8c91ed8e9db9ed
Brandon Aase <baase@catalyte.io>
2021-09-02 12:21:29 -0700

    Started solved check and added code for disabling Check button.

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from "react-redux";
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Buttons = (props) => {
+
+    const trayCandy = useSelector((state) => state.game.trayCandy);
+    const solutionCandy = useSelector((state) => state.game.solutionCandy);
+
+    const checkDisabled = () => {
+        let trayRow;
+        let rowsStatus = [false, false, false];
+        for (let i = 0; i < 3; i++) {
+            trayRow = trayCandy[i];
+            if (trayRow.indexOf('') === -1) {
+                rowsStatus[i] = true;
+            };
+        }
+
+        if (rowsStatus.indexOf(false) === -1) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    const checkSolved = () => {
+        if (JSON.stringify(trayCandy) === JSON.stringify(solutionCandy)) {
+            console.log('solved');
+        }
+    };
+
+    let disabled = checkDisabled();
+
     return (
         <View style={styles.buttons}>
             <View style={styles.buttonContainer}>
@@ -20,11 +50,13 @@ const Buttons = (props) => {
                         </View>
                     </TouchableOpacity>
                 </View>
-                <TouchableOpacity>
-                    <View style={styles.check}>
-                        <Icon name="checkmark-circle-outline" size={28} color='white' />
-                    </View>
-                </TouchableOpacity>
+                <View>
+                    <TouchableOpacity onPress={() => checkSolved()} disabled={disabled}>
+                        <View style={disabled ? [styles.check, styles.menuDisabled] : styles.check}>
+                            <Icon name="checkmark-circle-outline" size={28} color={disabled ? 'gray' : 'white'} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     )
@@ -53,6 +85,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'white',
         marginHorizontal: 10
+    },
+    menuDisabled: {
+        backgroundColor: '#d3d3d3'
     },
     order: {
         height: 50,
--- store/reducer/game.js
+++ store/reducer/game.js
@@ -13,6 +13,12 @@ const initialState = {
             ['', '', ''],
             ['', '', ''],
             ['', '', '']
+        ],
+    solutionCandy:
+        [
+            ['strawberry-circle', 'strawberry-triangle', 'strawberry-square'],
+            ['chocolate-circle', 'chocolate-triangle', 'chocolate-square'],
+            ['caramel-circle', 'caramel-triangle', 'caramel-square'],
         ]
 };
 
